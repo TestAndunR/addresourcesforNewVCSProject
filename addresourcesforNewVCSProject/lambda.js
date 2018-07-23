5c2c957c7d72d328f0383984657809943016dbeb
TestAndunR <andunranmal@gmail.com>
2018-07-23 09:38:30 +0530

    added a console log

--- addresourcesforNewVCSProject/lambda.js
+++ addresourcesforNewVCSProject/lambda.js
@@ -1,6 +1,6 @@
 let AWS = require('aws-sdk');
 
 exports.handler = function(event, context, callback) {
-    
+    console.log("Commit successfull for the same repo");
     callback(null,'Successfully executed');
 }
